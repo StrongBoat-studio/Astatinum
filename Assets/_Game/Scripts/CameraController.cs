da6bfe84217e8a89533036ad8abb7cbec46f898c
Żaneta Żurańska <zanetazuranska444@gmail.com>
2022-01-29 13:58:41 +0100

    Camera, Animacja Asta

---
+++ Assets/_Game/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform _Player;
+    [SerializeField] private float _Speed = 10f;
+    //SerializeField] private Vector3 _Distance;
+
+
+    private void Update()
+    {
+        transform.position= new Vector3(_Player.position.x, 8f, -13f);
+
+    }
+}
