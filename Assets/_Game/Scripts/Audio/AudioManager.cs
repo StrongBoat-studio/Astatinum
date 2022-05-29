289a55e4df88116e8e2cb78f0ae70c1eb5066194
Mateusz Lipiński <mastermat.1980@wp.pl>
2022-05-29 12:20:01 +0200

    Build fix

--- Assets/_Game/Scripts/Audio/AudioManager.cs
+++ Assets/_Game/Scripts/Audio/AudioManager.cs
@@ -42,27 +42,27 @@ public class AudioManager : MonoBehaviour
     }
 
     //Music
-    public void PlayMusicAmbience1() { if (_musicAmbience1.Path != null) RuntimeManager.PlayOneShot(_musicAmbience1); }
-    public void PlayMusicAmbience2() { if (_musicAmbience2.Path != null) RuntimeManager.PlayOneShot(_musicAmbience2); }
+    public void PlayMusicAmbience1()    { if (!_musicAmbience1.IsNull) RuntimeManager.PlayOneShot(_musicAmbience1); }
+    public void PlayMusicAmbience2()    { if (!_musicAmbience2.IsNull) RuntimeManager.PlayOneShot(_musicAmbience2); }
 
     //UI
-    public void PlayUIButtonHover() { if (_uiButtonHover.Path != null) RuntimeManager.PlayOneShot(_uiButtonHover); }
-    public void PlayUIButtonClick() { if (_uiButtonClick.Path != null) RuntimeManager.PlayOneShot(_uiButtonClick); }
+    public void PlayUIButtonHover()     { if (!_uiButtonHover.IsNull) RuntimeManager.PlayOneShot(_uiButtonHover); }
+    public void PlayUIButtonClick()     { if (!_uiButtonClick.IsNull) RuntimeManager.PlayOneShot(_uiButtonClick); }
 
     //Game
-    public void PlayGameCollect1() { if (_gameCollect1.Path != null) RuntimeManager.PlayOneShot(_gameCollect1); }
-    public void PlayGameCollect2() { if (_gameCollect2.Path != null) RuntimeManager.PlayOneShot(_gameCollect2); }
-    public void PlayGameCollect3() { if (_gameCollect3.Path != null) RuntimeManager.PlayOneShot(_gameCollect3); }
-    public void PlayGameCollect4() { if (_gameCollect4.Path != null) RuntimeManager.PlayOneShot(_gameCollect4); }
-    public void PlayGameCraftingOpen() { if (_gameCraftingOpen.Path != null) RuntimeManager.PlayOneShot(_gameCraftingOpen); }
-    public void PlayGameCraftingDone() { if (_gameCraftingDone.Path != null) RuntimeManager.PlayOneShot(_gameCraftingDone); }
-    public void PlayGameDoor1() { if (_gameDoor1.Path != null) RuntimeManager.PlayOneShot(_gameDoor1); }
-    public void PlayGameDoor2() { if (_gameDoor2.Path != null) RuntimeManager.PlayOneShot(_gameDoor2); }
-    public void PlayGameJournalOpen1() { if (_gameJournalOpen1.Path != null) RuntimeManager.PlayOneShot(_gameJournalOpen1); }
-    public void PlayGameJournalOpen2() { if (_gameJournalOpen2.Path != null) RuntimeManager.PlayOneShot(_gameJournalOpen2); }
-    public void PlayGameMar3KHappy() { if (_gameMar3KHappy.Path != null) RuntimeManager.PlayOneShot(_gameMar3KHappy); }
-    public void PlayGameMar3KStartup() { if (_gameMar3KStartup.Path != null) RuntimeManager.PlayOneShot(_gameMar3KStartup); }
-    public void PlayGameQuestBegin() { if (_gameQuestBegin.Path != null) RuntimeManager.PlayOneShot(_gameQuestBegin); }
+    public void PlayGameCollect1()      { if (!_gameCollect1.IsNull) RuntimeManager.PlayOneShot(_gameCollect1); }
+    public void PlayGameCollect2()      { if (!_gameCollect2.IsNull) RuntimeManager.PlayOneShot(_gameCollect2); }
+    public void PlayGameCollect3()      { if (!_gameCollect3.IsNull) RuntimeManager.PlayOneShot(_gameCollect3); }
+    public void PlayGameCollect4()      { if (!_gameCollect4.IsNull) RuntimeManager.PlayOneShot(_gameCollect4); }
+    public void PlayGameCraftingOpen()  { if (!_gameCraftingOpen.IsNull) RuntimeManager.PlayOneShot(_gameCraftingOpen); }
+    public void PlayGameCraftingDone()  { if (!_gameCraftingDone.IsNull) RuntimeManager.PlayOneShot(_gameCraftingDone); }
+    public void PlayGameDoor1()         { if (!_gameDoor1.IsNull) RuntimeManager.PlayOneShot(_gameDoor1); }
+    public void PlayGameDoor2()         { if (!_gameDoor2.IsNull) RuntimeManager.PlayOneShot(_gameDoor2); }
+    public void PlayGameJournalOpen1()  { if (!_gameJournalOpen1.IsNull) RuntimeManager.PlayOneShot(_gameJournalOpen1); }
+    public void PlayGameJournalOpen2()  { if (!_gameJournalOpen2.IsNull) RuntimeManager.PlayOneShot(_gameJournalOpen2); }
+    public void PlayGameMar3KHappy()    { if (!_gameMar3KHappy.IsNull) RuntimeManager.PlayOneShot(_gameMar3KHappy); }
+    public void PlayGameMar3KStartup()  { if (!_gameMar3KStartup.IsNull) RuntimeManager.PlayOneShot(_gameMar3KStartup); }
+    public void PlayGameQuestBegin()    { if (!_gameQuestBegin.IsNull) RuntimeManager.PlayOneShot(_gameQuestBegin); }
 
     //Play random
     public void PlayGameCollectRandom()
